956ae513d0db79e91f9e32d0c0b334f3c3968b8b
LavieMaru <laviemaru@gmail.com>
2017-01-12 23:03:10 -0600

    Add Day 2 challenge

---
+++ HackerRank30/Day2/Operators.js
@@ -0,0 +1,48 @@
+/*
+Objective 
+In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+Input Format
+
+There are  lines of numeric input: 
+The first line has a double,  (the cost of the meal before tax and tip). 
+The second line has an integer,  (the percentage of  being added as tip). 
+The third line has an integer,  (the percentage of  being added as tax).
+
+Output Format
+
+Print The total meal cost is totalCost dollars., where  is the rounded integer result of the entire bill ( with added tax and tip).
+
+Sample Input
+
+12.00
+20
+8
+Sample Output
+
+The total meal cost is 15 dollars.
+*/
+
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+	_input = _input.split(new RegExp('[\n ]+'));
+	main(+(_input[0]), +(_input[1]), +(_input[2]));    
+});
+
+function main(mealCost, tipPercent, taxPercent) {
+	var meal = Number(mealCost); 
+	    var tip = meal * (Number(tipPercent) / 100);
+	    var tax = meal * (Number(taxPercent) / 100);
+	    var total = meal + tip + tax;
+	    // Use console.log() to print to stdout
+	    console.log("The total meal cost is " + total.toFixed(0) + " dollars.")
+}
